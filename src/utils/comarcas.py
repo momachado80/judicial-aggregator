61cc0f0ff2dbacf8d1b3e8e6989970ae3d539237
momachado80 <momachado@gmail.com>
2025-11-15 17:45:50 -0300

    fix: adicionar funções extrair_codigo_comarca e formatar_numero_cnj

--- src/utils/comarcas.py
+++ src/utils/comarcas.py
@@ -380,3 +380,16 @@ def get_comarca_codigo(nome, tribunal="TJSP"):
         if comarca_nome.lower() == nome_lower:
             return codigo
     return None
+
+def extrair_codigo_comarca(numero_processo):
+    """Extrai código da comarca do número CNJ (posições 15-18)"""
+    if len(numero_processo) >= 20:
+        return numero_processo[14:18]
+    return None
+
+def formatar_numero_cnj(numero):
+    """Formata número no padrão CNJ: NNNNNNN-DD.AAAA.J.TR.OOOO"""
+    numero_limpo = ''.join(filter(str.isdigit, str(numero)))
+    if len(numero_limpo) == 20:
+        return f"{numero_limpo[0:7]}-{numero_limpo[7:9]}.{numero_limpo[9:13]}.{numero_limpo[13]}.{numero_limpo[14:16]}.{numero_limpo[16:20]}"
+    return numero
